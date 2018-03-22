8760265d3ca6c657f014d806651e09cec11181ad
Abhilash Reddy <abhilash.reddy@outlook.com>
2018-03-22 14:21:43 -0400

    Got Tab Events Firing!

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Movie } from './../shared/models/movie';
 import { MovieService } from './../services/movie.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ViewContainerRef } from '@angular/core';
+import { NgbTabset } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-home',
@@ -10,7 +11,24 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
   movies: Movie[];
   movies2: Movie[];
+  tabMovies: Movie[];
+  private tabSet: ViewContainerRef;
+
+  @ViewChild(NgbTabset) set content(content: ViewContainerRef) {
+    this.tabSet = content;
+  };
+
   constructor(private movieService: MovieService) { }
+
+  ngAfterViewInit() {
+    console.log('Tab Initialized');
+    console.log(this.tabSet);
+  }
+
+  selectedTab(tab) {
+    console.log('selected tab fired!');
+    console.log(tab);
+  }
 
   ngOnInit() {
     this.movieService.getNowPlayingMovies()
@@ -20,6 +38,11 @@ export class HomeComponent implements OnInit {
           this.movies2 = m.slice(6, 12);
         }
       );
+  }
+
+  tabSelected(x) {
+    console.log('Tab selected');
+    console.log(x);
   }
 
 }
